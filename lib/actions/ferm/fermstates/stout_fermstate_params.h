e3bf6160eebef45137c78330a2b21af0e620ca78
Alexander Chambers <alexander.chambers@hotmail.com>
2016-06-16 17:28:31 +0930

    Bali-boost smearing can be fractions of momentum now.

--- lib/actions/ferm/fermstates/stout_fermstate_params.h
+++ lib/actions/ferm/fermstates/stout_fermstate_params.h
@@ -41,7 +41,7 @@ namespace Chroma
 
     int            n_smear;
     Real           sm_fact;
-    multi1d<int>   momk;
+    multi1d<Real>   momk;
   };
 
   void read(XMLReader& xml, const std::string& path, MomentumFermStateParams& p);
